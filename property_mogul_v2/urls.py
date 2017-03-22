265a6aed66f61cf7989cbe7b15322dcd4622de3c
Philneeves <p.neeves@gmail.com>
2017-03-22 10:31:51 +0000

    Initial commit

---
+++ property_mogul_v2/urls.py
@@ -0,0 +1,32 @@
+"""property_mogul_v2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from members import views as members_views
+from home import views as home_views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', home_views.get_index, name='index'),
+    url(r'^register/$', members_views.register, name='register'),
+    url(r'^profile/$', members_views.profile, name='profile'),
+    url(r'^login/$', members_views.login, name='login'),
+    url(r'^logout/$', members_views.logout, name='logout'),
+    url(r'^cancel_subscription/$', members_views.cancel_subscription, name='cancel_subscription'),
+    url(r'^cancelled/$', members_views.cancel, name='cancel'),
+    url(r'^subscriptions_webhook/$', members_views.subscriptions_webhook, name='subscriptions_webhook'),
+]
