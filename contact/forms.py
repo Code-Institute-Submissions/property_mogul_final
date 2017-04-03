1f0588aabf64d0fa321b10bd5f72916bcf661e81
Philneeves <p.neeves@gmail.com>
2017-04-03 13:04:43 +0100

    Styling updates to HTML templates in line with theme

--- contact/forms.py
+++ contact/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
+    sender = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Your e-mail address here'}))
     cc_myself = forms.BooleanField(required=False)
