8c5bf879ac7346df0646d0e9a3fefa8717095cad
Philneeves <p.neeves@gmail.com>
2017-04-05 18:14:10 +0100

    trying to migrate tables and set up cleardb

--- settings/staging.py
+++ settings/staging.py
@@ -15,14 +15,17 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config("mysql://b47891a63528a0:144df2ac@eu-cdbr-west-01.cleardb.com/heroku_b2d78d537b51a5c")
+# DATABASES['default'] = dj_database_url.config("mysql://b47891a63528a0:144df2ac@eu-cdbr-west-01.cleardb.com/heroku_b2d78d537b51a5c")
+CLEAR_DB_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
+
+DATABASES['default'] = dj_database_url.parse(CLEAR_DB_URL)
 
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_jMrqEWUObXQYcsowwHCjDJ2E')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_lBEnGIqQGKUW4nXQh05GkxLE')
 
 
-SITE_URL = 'https://property-mogul.herokuapp.com'
+SITE_URL = 'https://property-mogul.herokuapp.com/'
 PAYPAL_NOTIFY_URL = 'https://property-mogul.herokuapp.com/a-really-hard-to-guess-url/'
 PAYPAL_RECEIVER_EMAIL = 'property.mogul@philip.com'
 PAYPAL_TEST = True
