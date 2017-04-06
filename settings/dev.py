530f9400e13b28e624026bbbd9f1d42435b43d7c
Philneeves <p.neeves@gmail.com>
2017-04-06 15:41:57 +0100

    changed credit card test details + disqus settings

--- settings/dev.py
+++ settings/dev.py
@@ -14,8 +14,8 @@ DATABASES = {
     }
 }
 
-# DISQUS_API_KEY = 'u7YpjuXW788cwHNjDlKu0n4zpYQozxqN1BE6yOpxjQwddBs5wYtUYTFs92C2zN5I'
-DISQUS_WEBSITE_SHORTNAME = 'nielsstackblog'
+DISQUS_API_KEY = 'u7YpjuXW788cwHNjDlKu0n4zpYQozxqN1BE6yOpxjQwddBs5wYtUYTFs92C2zN5I'
+DISQUS_WEBSITE_SHORTNAME = 'BlogApp'
 SITE_ID = 1
 
 # Stripe environment variables
